21118272fd83d626f32cb728efaeec4a8eaf45ee
Tobias Rasmussen <tobiasrasmussen90@gmail.com>
2024-04-29 22:00:52 +0200

    install git awesomeWM

--- install-nvim.sh
+++ install-nvim.sh
@@ -16,3 +16,6 @@ cd /home/$username/git-repos
 git clone https://github.com/neovim/neovim
 cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
 cd build && cpack -G DEB && sudo dpkg -i nvim-linux64.deb
+
+# rm -rf /home/$username/git-repos/neovim
+
--- install-setup.sh
+++ install-setup.sh
@@ -45,7 +45,20 @@ stow -S .
 chown -R $username:$username /home/$username
 
 # Install packges for desktop setup
-sudo apt install awesome sddm-theme-debian-maui -y
+sudo apt install xorg libxcb-xfixes0-dev sddm-theme-debian-maui -y
+
+cd /home/$username
+sudo apt build-dep awesome
+git clone https://github.com/awesomewm/awesome
+cd awesome
+make package
+cd build
+sudo apt install ./*.deb
+
+sudo mkdir /usr/share/xsessions
+sudo cp ${builddir}/awesome.desktop /usr/share/xsessions/
+
+# rm -rf /home/$username/awesome
 
 # Config Sddm to run on next login
 sudo dpkg-reconfigure sddm
